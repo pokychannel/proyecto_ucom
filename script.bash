c1798539a2e1b7aee9a343694fbaf1f7a6571fac
rubbini-en <91645742+rubbini-en@users.noreply.github.com>
2023-10-22 20:22:03 -0300

    Revert "add_dockerfile"

--- script.bash
+++
@@ -1,1 +0,0 @@
-docker run -p 8082:8088 -e DEBUG="True" -e DATABASE="Hospital-django" -e USER_DB="postgres" -e PASS_DB="12345" -e HOST_DB="192.168.13.53" -e PORT_DB="15432" --name finanzas diplomado-finanzas:1.4
