4048f7e4b7a696e5d64304a366b689ecfa703daa
brunofrutos <brunofrutos@gmail.com>
2023-10-22 21:06:30 -0300

    add app cuentas

--- finanzas/finanzas/settings.py
+++ finanzas/finanzas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps local
+    'applications.cuentas',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
